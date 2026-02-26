8ae788477bd018c8e19d87ea4d36d5a67c1be4d6
11aki2358 <f331dpprpl@gmail.com>
2026-02-26 14:20:41 +0900

    test2

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -2,6 +2,21 @@
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import useAuth from "../../utils/useAuth"
+import Link from "next/link";
+
+
+function insertAlink() {
+  var area = document.getElementById('text_area');
+  var text1 = ` <a href="`;
+  var text2 = '" target="_blank" rel="noreferrer noopener">link</a> ';
+
+  //カーソルの開始位置と終了位置を基準に分割
+  area.value = area.value.substr(0, area.selectionStart) +
+    text1 +
+    area.value.substr(area.selectionStart, area.selectionEnd - area.selectionStart) +
+    text2 +
+    area.value.substr(area.selectionEnd);
+}
 
 const CreateItem = () => {
   const [title, setTitle] = useState("")
@@ -14,7 +29,7 @@ const CreateItem = () => {
   const loginUseruserID = useAuth()
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/create`, {
         method: "POST",
@@ -43,15 +58,44 @@ const CreateItem = () => {
 
   if (loginUseruserID) {
     return (
-      <div>
-        <h1 className="page-title">アイテム作成</h1>
-        <form onSubmit={handleSubmit}>
-          <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" placeholder="タイトル" required />
-          
-          {/* <input value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required /> */}
-          <textarea value={description} onChange={(e) => setDescription(e.target.value)} name="description" rows={15} placeholder="記事" required></textarea>
-          <button>作成</button>
-        </form>
+
+      <div className="main">
+
+        <h2>投稿</h2>
+
+        <div>
+          <form onSubmit={handleSubmit}>
+
+            <div className="input_title">
+              <h3>タイトル</h3>
+              <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" placeholder="タイトル" required />
+            </div>
+
+            {/* <input value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required /> */}
+
+            <div className="input_main">
+              <h3>本文</h3>
+
+              <div className="insertAlink">
+                <div onClick={(e) =>
+                  insertAlink()
+                }>リンク挿入</div>
+              </div>
+
+              <textarea value={description} onChange={(e) => setDescription(e.target.value)} name="description" rows={15} placeholder="" id="text_area" required></textarea>
+            </div>
+
+            <button className="post_button">投稿</button>
+
+          </form>
+        </div>
+
+        <div className="singleArticle">
+          <div className="link_toAll">
+            <Link href={`../../`}>一覧に戻る</Link>
+          </div>
+        </div>
+
       </div>
     )
   }
