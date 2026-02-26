3f22e3b9316e2c78a44db6c1dd3bd33d8378d252
11aki2358 <f331dpprpl@gmail.com>
2026-02-26 09:02:18 +0900

    test2

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import connectDB from "../../../utils/database";
 import { ItemModel } from "../../../utils/schemaModels";
+import { Temporal } from 'temporal-polyfill';
 
 export async function POST(request) {
 
@@ -13,12 +14,23 @@ export async function POST(request) {
     // console.log(await request.json());
     await connectDB();
 
-    var now = new Date();
-    var nowTimeUTC = now.toUTCString();
-    var nowTimeJST = new Date(nowTimeUTC + 9 * 60 * 60 * 1000);
-    // var now_time = nowTime;
-    reqBody.postDate = `${nowTimeJST.getFullYear()}.${nowTimeJST.getMonth()}.${nowTimeJST.getDay()} ${nowTimeJST.getHours()}:${nowTimeJST.getMinutes()}:${nowTimeJST.getSeconds()}`;
-    
+    // var now = new Date();
+    // var nowTimeUTC = now.toUTCString();
+    // var nowTimeJST = new Date(nowTimeUTC + 9 * 60 * 60 * 1000);
+    // // var now_time = nowTime;
+    // reqBody.postDate = nowTimeJST.toLocaleString();
+
+    const now = Temporal.Now.instant(); //  ExactTime / タイムスタンプ
+    const TokyoTime = Temporal.Instant.from(now).toZonedDateTimeISO("Asia/Tokyo"); //  タイムゾーン(東京)の時刻に変換
+    reqBody.postDate = TokyoTime.toLocaleString("ja-jp", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+
     await ItemModel.create(reqBody);
 
     return NextResponse.json({ message: "アイテム作成成功" });
