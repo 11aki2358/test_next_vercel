557a100978026a951e24615a0b7637caa5f3895d
11aki2358 <f331dpprpl@gmail.com>
2026-03-01 11:05:20 +0900

    うーん

--- app/components/footer.js
+++ app/components/footer.js
@@ -5,10 +5,13 @@ const Footer = () => {
     <footer>
       <ul>
         <li>
-          <Link href="https://ryko-ryko.vercel.app/index.html" target="_blank">Ryko: Ryko(メインのサイト)</Link>
+          <Link href="https://ryko-ryko.vercel.app/index.html" target="_blank">Ryko: Ryko(メイン)</Link>
         </li>
         <li>
           <Link href="/user/login">ログイン</Link>
+        </li>
+        <li>
+          <Link href="/item/readsingle/69a392adbc86fac0248aa494">参考</Link>
         </li>
       </ul>
     </footer>
